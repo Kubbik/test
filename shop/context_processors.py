a2c2c1366ab1404d97b05bfc650d6854eecdf854
Aleksei <shurk24@gmail.com>
2024-08-28 14:25:28 +0700

    module

---
+++ shop/context_processors.py
@@ -0,0 +1,9 @@
+from .models import Category
+
+
+
+def categories(request):
+    categories = Category.objects.filter(parent=None)
+    return {'categories': categories}
+
+
---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import products_detail_view, products_view, category_list_view
+
+
+app_name = "shop"
+
+urlpatterns = [
+    path('', products_view, name='products'),
+    path('<slug:slug>/', products_detail_view, name='product_detail'),
+    path('search/<slug:slug>/', category_list_view, name='category_list'),
+]
